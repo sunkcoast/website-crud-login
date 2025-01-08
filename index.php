190c2a4b5cd487affc8cb068c67a19fffadebb7d
iBas <syncmebe@gmail.com>
2025-01-08 21:33:59 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>WEBSITE LOGIN</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
+    <!-- CSS -->
     <link rel="stylesheet" href="style.css" />
 </head>
 
@@ -23,7 +23,7 @@
 
     <!-- MAIN MENU Depan -->
     <main class="container text-center mt-5">
-    <img src="none.jpg" class="mb-4" style="max-width:50%; border-radius: 20%;">
+    <img src="gambardepan.jpg" class="mb-4" style="max-width:50%; border-radius: 20%;">
         <h2 class="mb-4">Halo, selamat datang di website kami.</h2>
         <nav>
             <a href="index.php" class="btn btn-dark btn-lg mx-2">Home</a>
